325c38ce2e9216c65b9f69f79b545d03ae258b1b
goodmean <80658449+goodmean@users.noreply.github.com>
2021-04-19 12:39:49 +0900

    2

--- Project6/Project6/sol.cpp
+++ Project6/Project6/sol.cpp
@@ -11,10 +11,23 @@ int main() {
 		scanf("%d", &input);
 
 		if (input == 1) {
+			for (int i = 0; ) {
 
+			}
 		}
 		else if (input == 2) {
-			
+			//사원추가
+			char* name = getString("사원명을 입력해주세요 : ");
+			int gender = getInt("성별을 입력해주세요 : ");
+			char* rank = getString("직급을 입력해주세요 : ");
+
+			Employee* e = new Employee(name, gender, rank);
+
+			delete[] name;
+			delete[] rank; // 동적으로 할당된 변수 삭제
+
+			employee[count] = e; // 등록
+			count++;
 		}
 		else if (input == 3) {
 
@@ -24,7 +37,25 @@ int main() {
 			break;
 		}
 		else {
-			printf("올바르지 않은 입력입니다.")
+			printf("올바르지 않은 입력입니다.");
 		}
 	}
+
+	return 0;
+}
+
+int getInt(const char* prompt) {
+	int input;
+	printf("%s", prompt);
+	fseek(stdin, 0, SEEK_END);
+	scanf("%d", &input);
+	return input;
+}
+
+char* getString(const char* prompt) {
+	char* input = new char[100];
+	printf("%s", prompt);
+	fseek(stdin, 0, SEEK_END);
+	scanf("%99[^\n]s", &input); // 99개 문자를 받아서 스트링으로 input에 저장
+	return input;
 }

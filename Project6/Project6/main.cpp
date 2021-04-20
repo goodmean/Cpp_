21b3e51d50f634bc48a64696b5ecb402b6a40bf1
goodmean <80658449+goodmean@users.noreply.github.com>
2021-04-20 10:53:55 +0900

    0420

--- Project6/Project6/main.cpp
+++ Project6/Project6/main.cpp
@@ -1,68 +1,75 @@
 #include "Header.h"
-#define true 1
 
 int main() {
-	
-	Employee* employee[100];
-	int count = 0; // 몇명의 사원이 있는지 기억
 
-	while (true) {
-		printf("명령어를 입력하세요\n");
-		printf("1. 사원 보기\n");
-		printf("2. 사원 추가\n");
-		printf("3. 사원 삭제\n");
-		printf("4. 프로그램 종료\n");
+    Employee* employee[100];
+    int count = 0;  //몇명의 사원이 있는지 기억
+    //사원보기
+    //사원추가
+    //사원삭제
+    //프로그램종료
 
-		fseek(stdin, 0, SEEK_END);
-		int input;
-		scanf("%d", &input);
+    while (true) {
+        printf("명령어를 입력하세요\n");
+        printf("1. 사원 보기\n");
+        printf("2. 사원 추가\n");
+        printf("3. 사원 삭제\n");
+        printf("4. 프로그램 종료\n");
 
-		if (input == 1) {
-			for (int i = 0; i < count; i++) {
-				employee[i]->printInfo();
-			}
-		}
-		else if (input == 2) {
-			//사원추가
-			char* name = getString("사원명을 입력해주세요 : ");
-			int gender = getInt("성별을 입력해주세요 : ");
-			char* rank = getString("직급을 입력해주세요 : ");
+        fseek(stdin, 0, SEEK_END);
+        int input;
+        scanf("%d", &input);
 
-			Employee* e = new Employee(name, gender, rank);
+        if (input == 1) {
+            //사원보기
+            for (int i = 0; i < count; i++) {
+                employee[i]->printInfo();
+            }
+        }
+        else if (input == 2) {
+            //사원추가
+            char* name = getString("사원명을 입력해주세요 :");
+            int gender = getInt("성별을 입력해주세요(0 남성 (1 여성 :");
+            char* rank = getString("직급을 입력해주세요 :");
 
-			delete[] name;
-			delete[] rank; // 동적으로 할당된 변수 삭제
+            Employee* e = new Employee(name, gender, rank);
 
-			employee[count] = e; // 등록
-			count++;
-		}
-		else if (input == 3) {
+            delete[] name;
+            delete[] rank;      //동적으로 할당된 변수 삭제
 
-		}
-		else if (input == 4) {
-			printf("프로그램을 종료합니다.");
-			break;
-		}
-		else {
-			printf("올바르지 않은 입력입니다.");
-		}
-	}
+            employee[count] = e;   //등록
+            count++;
+        }
+        else if (input == 3) {
+            //사원 삭제
+        }
+        else if (input == 4) {
+            //프로그램 종료
+            printf("프로그램을 종료합니다.");
+            break;
+        }
+        else {
+            //올바르지 않은 입력
+            printf("올바르지 않은 입력입니다.\n");
+        }
+    }
 
-	return 0;
+
+    return 0;
 }
 
 int getInt(const char* prompt) {
-	int input;
-	printf("%s", prompt);
-	fseek(stdin, 0, SEEK_END);
-	scanf("%d", &input);
-	return input;
+    int input;
+    printf("%s", prompt);
+    fseek(stdin, 0, SEEK_END);
+    scanf("%d", &input);
+    return input;
 }
 
 char* getString(const char* prompt) {
-	char* input = new char[100];
-	printf("%s", prompt);
-	fseek(stdin, 0, SEEK_END);
-	scanf("%99[^\n]s", &input); // 99개 문자를 받아서 스트링으로 input에 저장
-	return input;
+    char* input = new char[100];
+    printf("%s", prompt);
+    fseek(stdin, 0, SEEK_END);
+    scanf("%99[^\n]s", input);  //99개 문자를 받아서 스트링으로 input에 저장
+    return input;
 }

5504c18c3869abbb4e10b31a483c9964fa0b57c5
goodmean <80658449+goodmean@users.noreply.github.com>
2021-04-21 11:41:24 +0900

    0421

--- 0420/0420/Source.cpp
+++ 0420/0420/Source.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 
 class Animal { // 상속해주는 클래스의 멤버 변수, 함수를 외부에서도 활용할 수 있다.
-public:
+public: // public protected private
 	int legs;
 	char name[50];
 
@@ -15,9 +15,10 @@ public:
 
 	void printAnimalInfo() {
 		printf("다리의 개수 : %d\n", legs);
-		printf("이름 : %s", name);
+		printf("이름 : %s\n", name);
 	}
 };
+
 class Person : public Animal{
 public:
 
@@ -29,11 +30,24 @@ public:
 	char regist_no[30];
 };
 
+class Dog : public Animal {
+public:
+	Dog() : Animal(4, "강아지") {
+		printf("Dog 생성자 실행\n");
+	}
+};
+
 int main() { // 정적인 클래스보다 동적인 클래스에서 더 많이 사용한다.
 
-	Person* p = new Person("1234-9999");
+	Person* person = new Person("1234-9999");
 
-	p->printAnimalInfo();
+	person->printAnimalInfo();
+
+	Dog* dog = new Dog();
+	dog->printAnimalInfo();
+
+	//person = (Person*)animal;
+	person = (Person*)dog; //자식끼리의 데이터 구조가 완벽히 동일하면 성공
 
 
 	return 0;

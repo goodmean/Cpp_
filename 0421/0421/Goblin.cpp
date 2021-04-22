36b7e7a0c85e91a82dd2c35908a945b7f3a98915
goodmean <80658449+goodmean@users.noreply.github.com>
2021-04-22 12:54:41 +0900

    0422

--- 0421/0421/Goblin.cpp
+++ 0421/0421/Goblin.cpp
@@ -1,19 +1,31 @@
 #include "GHeader.h"
 
-class FantasyUnit {
-public:
-	int maxHp;
-	int atk;
-	int range;
-	int movementSpeed;
-};
+int main() {
+	FantasyUnit* units[10];
 
-class Goblin : public FantasyUnit {
-public:
-	Goblin() {
-		maxHp = 10;
-		atk = 5;
-		range = 10;
-		movementSpeed = 10;
+	srand(time(0));
+
+	for (int i = 0; i < 10; i++) {
+		switch (rand() % 4){
+		case 0:
+			units[i] = new Goblin();
+			break;
+		case 1:
+			units[i] = new Orc();
+			break;
+		case 2:
+			units[i] = new Slime();
+			break;
+		case 3:
+			units[i] = new SkeletonArchor();
+			break;
+		}
 	}
-};
+
+	for (int i = 0; i < 10; i++) {
+		units[i]->printInfo();
+	}
+
+
+	return 0;
+}

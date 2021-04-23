f52c6fd1b71f0cabbad58f34c853d8c7a240e80a
goodmean <80658449+goodmean@users.noreply.github.com>
2021-04-23 13:28:54 +0900

    0423

--- 0423/0423/Source.cpp
+++ 0423/0423/Source.cpp
@@ -3,24 +3,34 @@
 class Animal {
 public:
 	int legs;
-	char name[20];
+	char* name;
 
 	Animal(int legs, const char* name) {
+		this->name = new char[strlen(name) + 1];
 		this->legs = legs;
 		strcpy(this->name, name);
 	}
 
-	void printInfo() {
+	~Animal() {
+		delete[] name;
+	}
+
+	virtual void printInfo() { // 부모가 버추얼 함수면 자식은 아니어도 되지만 반대는 안됨.
 		printf("다리 %d, 이름 %s\n", legs, name);
 	}
 };
 
 class Person : public Animal {
 public:
-	char regist_no[40];
+	char* regist_no;
 
 	Person(const char* regist_no) : Animal(2, "사람") {
+		this->regist_no = new char[strlen(regist_no) + 1];
 		strcpy(this->regist_no, regist_no);
+	}
+
+	~Person() {
+		delete[] regist_no;
 	}
 
 	void printInfo() {
@@ -29,11 +39,18 @@ public:
 };
 
 int main() {
-	Person* person = new Person("1234-5678");
-	Animal* animal = person;
 
-	animal->printInfo();
-	person->printInfo();
+	Animal* animal = new Person("1234-5678");
+	delete animal;
+
+	Person* person = new Person("9876-5678");
+	delete person;
+
+	//Person* person = new Person("1234-5678");
+	//Animal* animal = person;
+
+	//animal->printInfo();
+	//person->printInfo();
 
 	return 0;
 }
---
+++ 0423/0423/Source1.cpp
@@ -0,0 +1,28 @@
+#include "Header.h"
+
+enum Gender {
+	MALE, FEMALE
+};
+
+class Student {
+public:
+	char name[10];
+	int student_no;
+	int gender;
+
+	Student(const char* name, int student_no, Gender gender) {
+		strcpy(this->name, name);
+		this->student_no = student_no;
+		this->gender = gender;
+	}
+
+	void printInfo() {
+		printf("%s의 학생번호는 %d입니다. 성별은 %s입니다.\n",
+			name, student_no, gender == Gender::MALE? "남성" : "여성");
+	}
+};
+
+int main() {
+
+	return 0;
+}

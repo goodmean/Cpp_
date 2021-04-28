22e2332ba103e826435fd57b55d3b241c827e42a
goodmean <80658449+goodmean@users.noreply.github.com>
2021-04-28 11:26:18 +0900

    0428

--- TEtrisExam/TEtrisExam/Header.h
+++ TEtrisExam/TEtrisExam/Header.h
@@ -9,4 +9,20 @@
 #define GRID_WIDTH 7
 #define GRID_HEIGHT 10
 
-int displayData = [GRID_HEIGHT][GRID_WIDTH] = { 0, };
+int displayData[GRID_HEIGHT][GRID_WIDTH] = { 0, };
+
+class Display {
+public:
+	void draw() {
+		for (int i = 0; i < GRID_HEIGHT; i++) {
+			for (int k = 0; k < GRID_WIDTH; k++) {
+				if (displayData[i][k] == 0) {
+					drawPosition(k, i, false);
+				}
+				else {
+					drawPosition(k, i, true);
+				}
+			}
+		}
+	}
+};

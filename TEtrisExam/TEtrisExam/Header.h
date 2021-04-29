78ab7c5bb7d317df30385586e661a82087b80a38
goodmean <80658449+goodmean@users.noreply.github.com>
2021-04-29 11:22:28 +0900

    느리게 떨어지게 했음

--- TEtrisExam/TEtrisExam/Header.h
+++ TEtrisExam/TEtrisExam/Header.h
@@ -44,12 +44,14 @@ class GameEngine {
 public:
 	int gameGridData[GRID_HEIGHT][GRID_WIDTH] = { 0, };
 	int userBlock[USERBLOCK_SIZE][USERBLOCK_SIZE] = {
-		{1, 1, 1},
+		{0, 1, 1},
 		{0, 0, 1},
 		{0, 0, 1}
 	};
 	int blockX = 0;
 	int blockY = 0;
+
+	float elapsed = 0.0f;
 
 	void init() {
 		// 최초에 게임엔진을 초기화 하는 과정
@@ -57,7 +59,12 @@ public:
 
 	void next(float dt, char keyboardInput) {
 		// 키보드 입력값을 받아와서 어떤 일을 할 지 결정(while 루프에서 매번 불려지는)
-		blockY++;
+		//blockY++;
+		elapsed = elapsed + dt; // elapsed += dt;
+		if (elapsed >= 0.5f) {
+			blockY++;
+			elapsed = elapsed - 0.5f; // elapsed -= dt;
+		}
 	}
 
 	void makeDisplayData() { // 실제 게임 데이터를 화면에 출력할 수 있는 데이터로 바꿔준다.

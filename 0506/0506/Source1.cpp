60b61b92d22232d689c0a4c67c5cb170976a81bb
goodmean <80658449+goodmean@users.noreply.github.com>
2021-05-07 11:44:27 +0900

    0507

--- 0506/0506/Source1.cpp
+++ 0506/0506/Source1.cpp
@@ -5,23 +5,67 @@
 #include <cstdio>
 #include <cstring>
 
+enum class Time {
+	Morning, Afternoon, Night
+};
+
 class Animal {
 public:
 	char name[50];
 	Animal(const char* name) {
 		strcpy(this->name, name);
 	}
-
+	// pure virtual func
 	virtual int getLegs() = 0;
 
 	void printInfo() {
-		printf("이름 : %s\n", name);
+		printf("이름 : %s / 다리 : %d\n", name, getLegs());
+	}
+};
+
+class Person : public Animal{
+private:
+	Time time = Time::Morning;
+public:
+	Person(const char* name) : Animal(name) {}
+
+	virtual int getLegs() {
+		switch (time){
+		case Time::Morning: 
+			return 4;
+		case Time::Afternoon: 
+			return 2;
+		case Time::Night: 
+			return 3;
+		default:
+			break;
+		}
+
+		return 2;
+	}
+	void setTime(Time t) {
+		this->time = t;
+	}
+};
+
+class Dog : public Animal {
+public:
+	Dog() : Animal("개") {}
+
+	virtual int getLegs() {
+		return 4;
 	}
 };
 
 int main() {
 
-	Animal* animal = new Animal("abcd");
+	//Animal* animal = new Animal("abcd");
+	Person* person = new Person("길동");
+	person->setTime(Time::Afternoon);
+	person->printInfo();
+
+	Dog* dog = new Dog();
+	dog->printInfo();
 
 	return 0;
 }
